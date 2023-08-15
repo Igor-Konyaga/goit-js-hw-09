7238c197123eb29060e44a6c574fffae53333124
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-08-16 01:57:38 +0300

    task 1 and 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startEl = document.querySelector('[data-start]');
+const stoptEl = document.querySelector('[data-stop]');
+const bodyEl = document.body;
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startEl.addEventListener('click', onColorChange);
+
+function onColorChange(e) {
+  timerId = setInterval(
+    () => (bodyEl.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  startEl.disabled = true;
+}
+
+stoptEl.addEventListener('click', stopColorChange);
+
+function stopColorChange() {
+  clearInterval(timerId);
+  startEl.disabled = false;
+}
+
 
---
+++ src/js/02-timer.js
@@ -0,0 +1,73 @@
+import flatpickr from 'flatpickr';
+import('flatpickr/dist/themes/dark.css');
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputEl = document.querySelector('[type="text"]');
+const btnEl = document.querySelector('[type="button"]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+let id = null;
+let userDate = null;
+
+btnEl.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() < Date.now()) {
+      window.alert('Please choose a date in the future');
+      userDate = null;
+    } else {
+      btnEl.disabled = false;
+      userDate = selectedDates[0];
+    }
+  },
+};
+
+flatpickr(inputEl, options);
+
+btnEl.addEventListener('click', onStartCountDown);
+
+function onStartCountDown(e) {
+  id = setInterval(() => {
+    let timeDifference = userDate - new Date();
+    let convert = convertMs(timeDifference);
+
+    btnEl.disabled = true;
+	 inputEl.disabled = true;
+
+    days.textContent = addLeadingZero(convert.days);
+    hours.textContent = addLeadingZero(convert.hours);
+    minutes.textContent = addLeadingZero(convert.minutes);
+    seconds.textContent = addLeadingZero(convert.seconds);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
