48cb7c9b0a0f46e2d5201f0c41226a40dd6d1337
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-08-16 02:19:09 +0300

    start task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });    
+}
+
+formEl.addEventListener('submit', onFormSumbmit);
+
+function onFormSumbmit(e) {
+  e.preventDefault();
+
+  let delayValue = delayEl.value;
+  const stepValue = stepEl.value;
+  const amountValue = amountEl.value;
+
+  // const intervalId = setInterval(() => {
+	for (let index = 1; index <= amountValue; index++) {
+	   createPromise(index, delayValue).then(({ position, delay }) =>
+      console.log(`✅ Fulfilled promise ${position} in ${delay} ms`)
+    )
+    .catch(({ position, delay }) =>
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+    );
+      delayValue+=stepValue
+	}
+   
+
+
+  formEl.reset();
 }
